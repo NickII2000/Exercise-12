e51e1ed78270db203742112685c7d71d2b59ceb7
Nick <Nick_II2000@mail.ru>
2022-04-04 23:10:00 +0600

    3-th commit

--- script.js
+++ script.js
@@ -32,10 +32,10 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Ещё один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Ещё один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
