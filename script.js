fb02a54ceff0bf823c233837948ea1d0df769e70
Nick <Nick_II2000@mail.ru>
2022-04-19 01:17:20 +0600

    9-th commit

--- script.js
+++ script.js
@@ -49,3 +49,19 @@ console.log(greeting);
 for (let i = 0; i < 5; i++) {
     console.log(i);
 }
+
+console.log(NaN || 2 || undefined);
+
+console.log(NaN && 2 && undefined);
+
+console.log(1 && 2 && 3);
+
+console.log(!1 && 2 || !3);
+
+console.log(25 || null && !3);
+
+console.log(NaN || null || !3 || undefined || 5);
+
+console.log(NaN || null && !3 && undefined || 5);
+
+console.log(5 === 5 && 3 > 1 || 5);
