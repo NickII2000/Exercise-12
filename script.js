a4d8e73d9fd7e2d2312482ad0030070def2e58fa
Nick <Nick_II2000@mail.ru>
2022-04-19 01:25:54 +0600

    11-th commit

--- script.js
+++ script.js
@@ -67,6 +67,7 @@ console.log(NaN || null && !3 && undefined || 5);
 console.log(5 === 5 && 3 > 1 || 5);
 
 
+
 const hamburger = 3;
 const fries = 3;
 const cola = 0;
@@ -74,5 +75,17 @@ const nuggets = 2;
 
 if (hamburger === 3 && cola || fries === 3 && nuggets) {
     console.log(hamburger === 3 && cola || fries === 3 && nuggets);
+    console.log('Done!');
+}
+
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger || cola || fries === 3 || nuggets) {
+    console.log(hamburger || cola || fries === 3 || nuggets);
     console.log('Done!');
 }
